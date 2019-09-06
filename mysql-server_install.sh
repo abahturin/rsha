34f472f395de647818cb04154f055e61830a4a2d
Aleksander Bahturin <aleksander.bahturin@khk.ee>
2019-09-06 11:35:08 +0300

    töötav script

--- mysql-server_install.sh
+++ mysql-server_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mysql-server=$(dpkg-query -W -f='${Status}' 'mysql-server' 2>/dev/null | grep -c "ok installed")
+mysql_server=$(dpkg-query -W -f='${Status}' 'mysql-server' 2>/dev/null | grep -c "ok installed")
 
-if [ '$mysql-server' -eq 0 ]; then
-	echo "Paigaldame mysql-server'i"
+if [ $mysql_server -eq 0 ]; then
+	echo "Paigaldame mysql-serveri"
 	apt-get install -y mysql-server;
 
-elif [ '$mysql-server' -eq 1 ]; then
+elif [ $mysql_server -eq 1 ]; then
 	echo "mysql-server on paigaldatud"
 #	service mysql-server status
 fi
